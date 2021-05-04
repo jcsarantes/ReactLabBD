d62c3532a2b5107af56fdfe713313a1a90f8ca1d
jcsarantes <prof.janaine@gmail.com>
2021-05-04 08:27:35 -0300

    Exemplos 09 e 10

--- Aula08/app09/src/App.js
+++ Aula08/app09/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MeuFormulario extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      descricao: 'O conteúdo de uma textarea deve ser inserido no atributo value.'
+    };
+  }
+
+  render() {
+    return(
+      <form>
+        <textarea value={this.state.descricao} rows="4" cols="20" />
+      </form>
+    );
+  }
 }
 
-export default App;
+export default MeuFormulario;
